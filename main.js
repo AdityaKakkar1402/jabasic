485106502fe6ffeef3e8eba9e56938c362f692cc
AdityaKakkar1402 <kakkar.adi14@gmail.com>
2022-07-23 23:25:29 +0530

    hello

---
+++ main.js
@@ -0,0 +1,120 @@
+// console.log("aditya");
+// console.log(typeof("aditya"));
+// let k;
+// console.log(k);
+// k="aditya"
+// console.log(typeof(k));
+// let t=4;
+// console.log(typeof(t));
+// t=4.1;
+// console.log(typeof(t));
+// let p=k+t;
+// console.log(p);
+// console.log(typeof(p));
+// let myName="adi"
+// console.log(myName===k);
+// console.log(myName!==k);
+// alert("hello")
+
+
+
+// let myVar="aditya";
+// console.log(myVar.length);
+// console.log("home".length);
+// console.log(myVar.charAt(3));
+// console.log(myVar.indexOf("t"));
+// console.log(myVar.lastIndexOf("a"));
+// console.log(myVar.slice(3));
+// console.log(myVar.toUpperCase());
+// console.log(myVar.split("t"));
+// console.log(myVar.split(""));
+// let reply=`here is ${myVar}`
+// console.log(reply);
+
+// let a =2;
+// a=4
+// console.log(a);
+
+
+// var t=0;
+// var t=3;
+// t=2
+// console.log(t);
+
+// let p=0;
+// let p=4;
+// p=4;
+// console.log(p);
+
+// const one=42;
+// const float=42.2466;
+// const string1="42.03";
+// console.log(one);
+// console.log(one===string1);
+// console.log(typeof(float)===typeof(one));
+// let q=Number(string1);
+// console.log(typeof(q));
+// console.log(Number("hello"));
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number.isInteger(string1));
+// console.log(Number.parseFloat(string1));
+// console.log(Number.parseFloat(float).toFixed(2));
+// console.log(Number.parseInt(string1));
+// console.log(typeof(float.toString()));
+
+
+// console.log(Math.PI);
+// let p=3.67;
+// console.log(Math.trunc(p));
+// console.log(Math.trunc(Math.PI));
+// console.log(Math.round(Math.PI));
+// console.log(Math.ceil(Math.PI));
+// console.log(Math.floor(Math.PI));
+// console.log(Math.pow(2,4));
+// console.log(Math.min(2,0.5,9));
+// console.log(Math.random());
+// console.log(Math.ceil(Math.random()*3));
+// console.log(Math.random()*3);
+
+
+//ternary
+// let soup="yes";
+// let res=soup?"yes,we have":"sorry";
+// console.log(res);
+
+
+let marks=prompt("enter marks")
+console.log(marks);
+console.log(marks??"marks not entered");
+let result=marks>90?"faar diye":marks>40?"pass":marks>30?"bura hua":"kuch nai ho sakta"
+console.log(result);
+
+
+
+// game to guess the Number
+
+// game to play hand cricket with computer
+
+
+// let name=prompt("enter name")
+// console.log(name.trim());
+// console.log(name);
+
+
+// let var difference
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
